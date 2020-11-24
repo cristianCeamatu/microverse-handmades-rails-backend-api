7d746b59583092edb5ab56bcb5752ee8c3f43990
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-11-24 16:12:21 +0200

    Fix tests and default image for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,54 @@
 class Item < ApplicationRecord
+
   include Rails.application.routes.url_helpers
 
+
+
   belongs_to :user
+
+
+
   has_many :favorite_items, dependent: :destroy
+
+
+
   has_many :favorited_by, through: :favorite_items, source: :user, dependent: :destroy
+
+
+
   has_one_attached :image
 
+
+
   validates :name, presence: true, length: { minimum: 2, maximum: 40 }
+
+
+
   validates :description, presence: true, length: { minimum: 6, maximum: 50_000 }
+
+
+
   validates :price, presence: true, length: { maximum: 12 }
+
+
+
   validates :usedFor, presence: true, length: { minimum: 2, maximum: 60 }
-  validates :image, presence: true
+
+
 
   def set_host
+
     Rails.env.production? ? 'https://handmades-rails-api-backend.herokuapp.com' : 'http://localhost:3000'
+
   end
 
+
+
   def image_url
-    image.nil? ? nil : rails_representation_url(image.variant(resize: '400x600'), host: set_host)
+
+    image.nil? ? "<img src='http://loremflickr.com/600/800/man' alt='Man'/>" : rails_representation_url(image.variant(resize: '600x800'), host: set_host)
+
   end
+
 end
+
--- spec/models/favorite_item_spec.rb
+++ spec/models/favorite_item_spec.rb
@@ -3,18 +3,22 @@ require 'rails_helper'
 RSpec.describe FavoriteItem, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
+
     it { should belong_to(:item) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:user_id) }
+
     it { is_expected.to validate_presence_of(:item_id) }
   end
 
   describe 'scopes' do
     before :each do
       @user = FactoryBot.create(:user)
+
       @item = @user.items.create!(name: 'Example', description: 'Example desx', price: 55.35, usedFor: 'Example use')
+
       @favorite_item = @user.favorite_items.create!(item_id: @item.id)
     end
 
