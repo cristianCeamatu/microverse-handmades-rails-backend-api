fbd71510ff25c1e45e776c8b53331853ac4c8ed3
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-11-26 13:16:57 +0200

    Add email regex validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:nickname) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to allow_value('email@addresse.foo').for(:email) }
+    it { is_expected.to_not allow_value('foo').for(:email) }
   end
 
   describe 'scopes' do
